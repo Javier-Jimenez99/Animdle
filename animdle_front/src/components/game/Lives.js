20947225038ea5ea31a11ffb811a1c5cf4db8ac6
Javier-Jimenez99 <javijj99@gmail.com>
2023-09-10 16:53:36 +0200

    fix heart sizes

--- animdle_front/src/components/game/Lives.js
+++ animdle_front/src/components/game/Lives.js
@@ -11,24 +11,24 @@ function Lives({ livesUsed, gameState, hardcore = false, maxLives = 5 }) {
         console.log(livesUsed, maxLives, gameState, hardcore);
         let hearts = [];
         for (let i = 0; i < maxLives - livesUsed; i++) {
-            hearts.push(<img className="heart" width={"15%"} src={heartIcon} alt="Live Heart of lost tries" key={hearts.length} />);
+            hearts.push(<img className="heart" width={"13%"} src={heartIcon} alt="Live Heart of lost tries" key={hearts.length} />);
         }
 
         let loopStart = 0;
         if (gameState === "win") {
-            hearts.push(<img className="heart" width={"15%"} src={heartGreenIcon} alt="Live Heart meaning that you won" key={hearts.length} />);
+            hearts.push(<img className="heart" width={"13%"} src={heartGreenIcon} alt="Live Heart meaning that you won" key={hearts.length} />);
             loopStart++;
         }
 
         for (let i = loopStart; i < livesUsed; i++) {
-            hearts.push(<img className="heart dead-heart" width={"15%"} src={heartIcon} alt="Live Heart of remaining tries" key={hearts.length} />);
+            hearts.push(<img className="heart dead-heart" width={"13%"} src={heartIcon} alt="Live Heart of remaining tries" key={hearts.length} />);
         }
 
         if (hardcore) {
             if (maxLives === 5) {
                 hearts.push(
                     <Tooltip title={"Win no hardcore mode to unlock this heart"} followCursor>
-                        <img className="heart" width={"15%"} src={heartGoldBlockedIcon} alt="Live Heart of remaining tries" key={hearts.length} />
+                        <img className="heart" width={"13%"} src={heartGoldBlockedIcon} alt="Live Heart of remaining tries" key={hearts.length} />
                     </Tooltip>
                 );
             }
